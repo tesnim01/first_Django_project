9b804b152ac16377b1a43a24c9550c190e4bfd80
tesnim <tesnimelbehi@gmail.com>
2024-01-27 23:04:34 +0100

    pages urls commit

--- pages/pages/views.py
+++ pages/pages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+class HomePageView(TemplateView): #instead of def we create class "classes sshould be capitalized"
+ template_name = "home.html"
+
+'''
+from django.http import HttpResponse
+def homePageView(request):
+    return HttpResponse("Hello, World!")'''
